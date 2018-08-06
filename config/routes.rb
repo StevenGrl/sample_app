ce390f3eab67975a7081d5a0b032b133d0f3fb4a
StevenGrl <steven.grillon.45@gmail.com>
2018-08-06 11:58:39 +0200

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get  '/help',  to: 'static_pages#help', as: 'help'
   get  '/about',  to: 'static_pages#about', as: 'about'
   get  '/contact',  to: 'static_pages#contact', as: 'contact'
+  post '/signup',  to: 'users#create'
+  resources :users
 end
